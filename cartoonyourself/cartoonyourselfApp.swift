37770ddad5b085b4d3e0356ac115f355ac1b044a
Julian Beck <ju-fa-beck@t-online.de>
2025-04-03 20:41:55 +0200

    Initial Commit

---
+++ cartoonyourself/cartoonyourselfApp.swift
@@ -0,0 +1,17 @@
+//
+//  cartoonyourselfApp.swift
+//  cartoonyourself
+//
+//  Created by Julian Beck on 03.04.25.
+//
+
+import SwiftUI
+
+@main
+struct cartoonyourselfApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
